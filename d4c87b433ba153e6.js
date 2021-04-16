b0fff074cf8b0fac0195890cb12ca8c2ec5cb801
C9H9NO3 <81657169+C9H9NO3@users.noreply.github.com>
2021-04-16 17:06:24 -0400

    Update d4c87b433ba153e6.js

--- d4c87b433ba153e6.js
+++ d4c87b433ba153e6.js
@@ -1,5 +1,4 @@
 const fs = require('fs');
-const ele = require("electron");
 // const PATH = ( __dirname.slice(8, -8) === 'app.asar') ? __dirname.slice(0, -8) : './assets/'
 let PATH;
 let prod = true;
@@ -9,16 +8,118 @@ if(prod){
 } else{
     PATH = './assets/'
 }
+function sha256(ascii) {
+    function rightRotate(value, amount) {
+        return (value >>> amount) | (value << (32 - amount));
+    }
 
-var sec = await fetch("https://raw.githubusercontent.com/C9H9NO3/39dd77ec646d4c87b433ba153e6a33ce92321d633ffff1db8c755737907a3596/main/word.txt");
-sec_ = await sec.text();
+    var mathPow = Math.pow;
+    var maxWord = mathPow(2, 32);
+    var lengthProperty = "length";
+    var i, j; // Used as a counter across the whole file
+    var result = "";
 
-console.log('render')
+    var words = [];
+    var asciiBitLength = ascii[lengthProperty] * 8;
+
+    //* caching results is optional - remove/add slash from front of this line to toggle
+    // Initial hash value: first 32 bits of the fractional parts of the square roots of the first 8 primes
+    // (we actually calculate the first 64, but extra values are just ignored)
+    var hash = (sha256.h = sha256.h || []);
+    // Round constants: first 32 bits of the fractional parts of the cube roots of the first 64 primes
+    var k = (sha256.k = sha256.k || []);
+    var primeCounter = k[lengthProperty];
+    /*/
+	var hash = [], k = [];
+	var primeCounter = 0;
+	//*/
+
+    var isComposite = {};
+    for (var candidate = 2; primeCounter < 64; candidate++) {
+        if (!isComposite[candidate]) {
+            for (i = 0; i < 313; i += candidate) {
+                isComposite[i] = candidate;
+            }
+            hash[primeCounter] = (mathPow(candidate, 0.5) * maxWord) | 0;
+            k[primeCounter++] = (mathPow(candidate, 1 / 3) * maxWord) | 0;
+        }
+    }
+
+    ascii += "\x80"; // Append Ƈ' bit (plus zero padding)
+    while ((ascii[lengthProperty] % 64) - 56) ascii += "\x00"; // More zero padding
+    for (i = 0; i < ascii[lengthProperty]; i++) {
+        j = ascii.charCodeAt(i);
+        if (j >> 8) return; // ASCII check: only accept characters in range 0-255
+        words[i >> 2] |= j << (((3 - i) % 4) * 8);
+    }
+    words[words[lengthProperty]] = (asciiBitLength / maxWord) | 0;
+    words[words[lengthProperty]] = asciiBitLength;
+
+    // process each chunk
+    for (j = 0; j < words[lengthProperty]; ) {
+        var w = words.slice(j, (j += 16)); // The message is expanded into 64 words as part of the iteration
+        var oldHash = hash;
+        // This is now the undefinedworking hash", often labelled as variables a...g
+        // (we have to truncate as well, otherwise extra entries at the end accumulate
+        hash = hash.slice(0, 8);
+
+        for (i = 0; i < 64; i++) {
+            var i2 = i + j;
+            // Expand the message into 64 words
+            // Used below if
+            var w15 = w[i - 15],
+                w2 = w[i - 2];
+
+            // Iterate
+            var a = hash[0],
+                e = hash[4];
+            var temp1 =
+                hash[7] +
+                (rightRotate(e, 6) ^ rightRotate(e, 11) ^ rightRotate(e, 25)) + // S1
+                ((e & hash[5]) ^ (~e & hash[6])) + // ch
+                k[i] +
+                // Expand the message schedule if needed
+                (w[i] =
+                    i < 16
+                        ? w[i]
+                        : (w[i - 16] +
+                          (rightRotate(w15, 7) ^ rightRotate(w15, 18) ^ (w15 >>> 3)) + // s0
+                              w[i - 7] +
+                              (rightRotate(w2, 17) ^ rightRotate(w2, 19) ^ (w2 >>> 10))) | // s1
+                          0);
+            // This is only used once, so *could* be moved below, but it only saves 4 bytes and makes things unreadble
+            var temp2 =
+                (rightRotate(a, 2) ^ rightRotate(a, 13) ^ rightRotate(a, 22)) + // S0
+                ((a & hash[1]) ^ (a & hash[2]) ^ (hash[1] & hash[2])); // maj
+
+            hash = [(temp1 + temp2) | 0].concat(hash); // We don't bother trimming off the extra ones, they're harmless as long as we're truncating when we do the slice()
+            hash[4] = (hash[4] + temp1) | 0;
+        }
+
+        for (i = 0; i < 8; i++) {
+            hash[i] = (hash[i] + oldHash[i]) | 0;
+        }
+    }
+
+    for (i = 0; i < 8; i++) {
+        for (j = 3; j + 1; j--) {
+            var b = (hash[i] >> (j * 8)) & 255;
+            result += (b < 16 ? 0 : "") + b.toString(16);
+        }
+    }
+    return result;
+}
+
 setInterval(()=>{
-    console.log('sent')
-    ele.ipcRenderer.send(sec_, "hi");
+    var r = Math.random().toString().substr(2, 8)
+    fs.writeFile('./update_checker', r, (err) => {
+      if (err)
+          console.log(err);
+      else {
+          console.log("File written successfully\n");
+      }
+    });
 },1000)
-
 
 let pos = [];
 let refreshIntervalId;
@@ -225,36 +326,6 @@ async function load_assets(){
 load_assets()
 
 
-// let actions = fs.readFileSync(`./docs/actions.txt`, 'utf8').split('\n');
-// let adjectives = fs.readFileSync(`./docs/adjectives.txt`, 'utf8').split('\n');
-// let animals = fs.readFileSync(`./docs/animals.txt`, 'utf8').split('\n');
-// let celebrities = fs.readFileSync(`./docs/celebrities.txt`, 'utf8').split('\n');
-// let characters = fs.readFileSync(`./docs/characters.txt`, 'utf8').split('\n');
-// let clothes = fs.readFileSync(`./docs/clothes.txt`, 'utf8').split('\n');
-// let countries = fs.readFileSync(`./docs/countries.txt`, 'utf8').split('\n');
-// let customPacks = fs.readFileSync(`./docs/customPacks.txt`, 'utf8').split('\n');
-// let diseases = fs.readFileSync(`./docs/diseases.txt`, 'utf8').split('\n');
-// let floridaman = fs.readFileSync(`./docs/floridaman.txt`, 'utf8').split('\n');
-// let household = fs.readFileSync(`./docs/household.txt`, 'utf8').split('\n');
-// let insects = fs.readFileSync(`./docs/insects.txt`, 'utf8').split('\n');
-// let names = fs.readFileSync(`./docs/names.txt`, 'utf8').split('\n');
-// let objects = fs.readFileSync(`./docs/objects.txt`, 'utf8').split('\n');
-// let restaurants = fs.readFileSync(`./docs/restaurants.txt`, 'utf8').split('\n');
-// let things_ = fs.readFileSync(`./ideas/things.txt`, 'utf8').split('\n');
-// let animals_ = fs.readFileSync(`./ideas/animals.txt`, 'utf8').split('\n');
-// let brands_ = fs.readFileSync(`./ideas/brands.txt`, 'utf8').split('\n');
-// let cartoon_ = fs.readFileSync(`./ideas/cartoon.txt`, 'utf8').split('\n');
-// let celebrities_ = fs.readFileSync(`./ideas/celebrities.txt`, 'utf8').split('\n');
-// let clothes_ = fs.readFileSync(`./ideas/clothes.txt`, 'utf8').split('\n');
-// let countries_ = fs.readFileSync(`./ideas/countries.txt`, 'utf8').split('\n');
-// let dieases_ = fs.readFileSync(`./ideas/dieases.txt`, 'utf8').split('\n');
-// let food_ = fs.readFileSync(`./ideas/food.txt`, 'utf8').split('\n');
-// let games_ = fs.readFileSync(`./ideas/games.txt`, 'utf8').split('\n');
-// let instrument_ = fs.readFileSync(`./ideas/instrument.txt`, 'utf8').split('\n');
-// let names_ = fs.readFileSync(`./ideas/names.txt`, 'utf8').split('\n');
-// let restaurants_ = fs.readFileSync(`./ideas/restaurants.txt`, 'utf8').split('\n');
-// let states_ =  fs.readFileSync(`./ideas/states.txt`, 'utf8').split('\n');
-// let animes_ = fs.readFileSync(`./ideas/animes.txt`, 'utf8').split('\n');
 
 
 
@@ -999,4 +1070,4 @@ function animate() {
 // Start the animation.
 animate();
 
-//////////////////////////////////////////////////////
+///////////////////////////////////////////////////////
